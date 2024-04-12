0170490d2c51834d785fcef00ed118605df90549
hashemhassen <hashemhassen0@gmail.com>
2024-04-13 00:54:04 +0200

    adding styles & assets

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+abstract class AppStyles{
+  static const TextStyle styleRegular22 = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Poppins",
+    color: Colors.black,
+  );
+  static const TextStyle styleMedium14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    fontFamily: "Poppins",
+    color: Color(0xff6B4B3E),
+  );
+  static const TextStyle styleMedium16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: "Poppins",
+    color: Color(0xff6B4B3E),
+  );
+  static const TextStyle styleRegular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Poppins",
+    color: Color(0xff6F6460),
+  );
+  static const TextStyle styleMedium11 = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w500,
+    fontFamily: "Poppins",
+    color: Color(0xff6B4B3E),
+  );
+  static const TextStyle styleRegular10 = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Poppins",
+    color: Color(0xff6B4B3E),
+  );
+  static const TextStyle styleRegular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Heebo",
+    color: Color(0xff6B4B3E),
+  );
+  static const TextStyle styleMedium18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    fontFamily: "Poppins",
+    color: Color(0xffFFFFFF),
+  );
+  static const TextStyle styleRegular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Poppins",
+    color: Color(0xff07122E),
+  );
+  static const TextStyle styleMedium24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    fontFamily: "Poppins",
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleLight16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
+    fontFamily: "Poppins",
+    color: Color(0xff000000),
+  );
+
+}
